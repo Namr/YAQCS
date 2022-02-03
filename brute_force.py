50373e93aa16c378a6dca43e9736aca0203fdbd9
Amr Moussa <amrmoussa100@gmail.com>
2022-02-03 12:42:27 -0500

    added MPI parallelism

--- brute_force.py
+++ brute_force.py
@@ -1,77 +1,83 @@
-from qiskit import QuantumCircuit, assemble, Aer, QuantumRegister, ClassicalRegister, transpile
+from yaqcs import RXGate, RXXGate, Circuit
 from math import pi, sqrt
-from qiskit.visualization import plot_bloch_multivector, plot_histogram, array_to_latex
-import itertools
 import copy
 import time
+import itertools
+import numpy as np
 
-allowed_gates = [("rxx", pi / 2), ("rx", pi / 2), ("rx", -pi / 2),
-                 ("ry", pi / 2), ("ry", -pi / 2), ("rz", pi / 2),
-                 ("rz", -pi / 2)]
+USING_MPI = False
+if USING_MPI:
+    import threading
+    from mpi4py import MPI
 
-sim = Aer.get_backend('aer_simulator_unitary')
-target_unitary = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
-count = 0
-sTime = time.time()
+allowed_gates = [
+    RXXGate([pi / 2]),
+    RXGate([pi / 2]),
+    RXGate([-pi / 2]),
+]
+
+#target_unitary = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
+target_unitary = [[
+    0.35355 - 0.35355j, -0.35355 + 0.35355j, 0.35355 - 0.35355j,
+    0.35355 - 0.35355j
+],
+                  [
+                      -0.35355 + 0.35355j, 0.35355 - 0.35355j,
+                      0.35355 - 0.35355j, 0.35355 - 0.35355j
+                  ],
+                  [
+                      0.35355 - 0.35355j, 0.35355 - 0.35355j,
+                      0.35355 - 0.35355j, -0.35355 + 0.35355j
+                  ],
+                  [
+                      0.35355 - 0.35355j, 0.35355 - 0.35355j,
+                      -0.35355 + 0.35355j, 0.35355 - 0.35355j
+                  ]]
 
 
 def test_circuit(qubits: int, circuit: list) -> bool:
-    c = QuantumCircuit(qubits)
-    for op in circuit:
-        if op[0][0] == "rxx":
-            c.rxx(op[0][1], op[1], op[1] + 1)
-        if op[0][0] == "rx":
-            c.rx(op[0][1], op[1])
-        if op[0][0] == "ry":
-            c.ry(op[0][1], op[1])
-        if op[0][0] == "rz":
-            c.rz(op[0][1], op[1])
+    c = Circuit(qubits, qiskit_style=False)
+    for gate in circuit:
+        if gate[0].num_qbits > qubits - gate[1]:
+            return False
+        c.append_gate(gate[0], gate[1])
 
-    # see if the unitary is the same
-    c.save_unitary()
-    transpiled_qc = transpile(c, sim)
-    result = sim.run(transpiled_qc).result().get_unitary(transpiled_qc)
-
-    if (result == target_unitary).all():
+    if (np.around(c.get_unitary(), 3) == np.around(target_unitary, 3)).all():
+        print("============================================")
+        print("found a solution with the following unitary")
+        print(c.get_unitary())
+        print("created with this circuit")
         print(circuit)
-        print("we got em")
-        quit()
+        print("============================================")
         return True
     return False
 
 
-def pure_brute_force(qubits: int, gates: list, max_depth: int,
-                     current_depth: int, circuit: list) -> bool:
+def pure_brute_force(qubits: int,
+                     gates: list,
+                     max_depth: int,
+                     stride: int = 1) -> bool:
 
-    global count
-    global time
+    gates_with_positions = list(itertools.product(allowed_gates,
+                                                  range(qubits)))
 
-    if current_depth > max_depth:
-        return False
+    count = 0
+    for circuit in itertools.product(gates_with_positions, repeat=max_depth):
+        count = count + 1
+        if count == stride:
+            count = 0
+            if test_circuit(qubits, circuit):
+                return True
 
-    for q in range(qubits):
-        for gate in allowed_gates:
-            if gate[0] == "rxx" and q == qubits - 1:
-                continue
-
-            # save the state of our circuit before our modification
-            save_old = copy.deepcopy(circuit)
-
-            # add the new gate and see if it makes it work
-            circuit.append((gate, q))
-            test_circuit(qubits, circuit)
-
-            count += 1
-            if count % 1000 == 0:
-                print(count)
-                print(time.time() - sTime)
-
-            # if not then recursively add a new gate
-            if pure_brute_force(qubits, gates, max_depth, current_depth + 1,
-                                circuit) == False:
-                circuit = save_old
+    return False
 
 
 if __name__ == "__main__":
-    if pure_brute_force(2, allowed_gates, 3, 0, []) == False:
-        print("no solutions?")
+    if USING_MPI:
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+
+    pure_brute_force(2, allowed_gates, 3, stride=rank - 1)
+
+    if USING_MPI:
+        comm.Barrier()
