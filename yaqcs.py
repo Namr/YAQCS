0583c81abf552c772ea8a389b7f67d68b310e5c5
Amr Moussa <amrmoussa100@gmail.com>
2022-02-03 12:38:53 -0500

    fixed order of operations bug

--- yaqcs.py
+++ yaqcs.py
@@ -20,13 +20,18 @@ class Circuit:
     num_qbits = 0
     # an operation is a tuple of (gate, position_placed), this is also known as a "placed_gate"
     operations = []
+    is_qiskit_ordering = False
 
-    def __init__(self, qbits):
+    def __init__(self, qbits, qiskit_style=False):
         self.num_qbits = qbits
+        self.is_qiskit_ordering = qiskit_style
 
     def append_gate(self, gate: Gate, position):
+        if self.is_qiskit_ordering:
+            position = abs(position - (self.num_qbits - 1))
+
         if gate.num_qbits > self.num_qbits - position:
-            print("cant add a multiqbit gate at position " + position)
+            print("cant add a multiqbit gate at position " + str(position))
             return None
 
         self.operations.append((gate, position))
@@ -50,50 +55,98 @@ class Circuit:
             print("Can't get Unitary of a circuit with no operations")
             return None
 
-        unitary = self.get_displaced_gate_unitary(self.operations[0])
-        if (len(self.operations) > 1):
-            for op in range(1, len(self.operations)):
-                unitary *= self.get_displaced_gate_unitary(self.operations[op])
+        true_operations = self.operations[::-1]
+
+        unitary = self.get_displaced_gate_unitary(true_operations[0])
+        if (len(true_operations) > 1):
+            for op in range(1, len(true_operations)):
+                unitary = unitary * \
+                    self.get_displaced_gate_unitary(true_operations[op])
         return unitary
 
 
 class RXGate(Gate):
-    def __init__(self) -> None:
+    def __init__(self, params=[math.pi / 2.0]) -> None:
         self.num_qbits = 1
         self.num_params = 1
-        self.num_variations = 1
-        self.variations = [math.pi / 2.0, -math.pi / 2.0]
+        if (len(params) != self.num_params):
+            print("invalid number of params in RXGate")
+
+        self.params = params
+
         return None
 
-    def get_unitary(self, variation_index: int = 0) -> np.ndarray:
-        ct = np.cos(self.variations[variation_index] / 2.0)
-        st = np.sin(self.variations[variation_index] / 2.0)
+    def get_unitary(self) -> np.ndarray:
+        ct = np.cos(self.params[0] / 2.0)
+        st = np.sin(self.params[0] / 2.0)
 
         return np.matrix([[ct, -1j * st], [-1j * st, ct]])
 
 
-class RXXGate(Gate):
-    def __init__(self) -> None:
-        self.num_qbits = 2
+class RYGate(Gate):
+    def __init__(self, params=[math.pi / 2.0]) -> None:
+        self.num_qbits = 1
         self.num_params = 1
-        self.num_variations = 1
-        self.variations = [math.pi / 2.0, -math.pi / 2.0]
+        if (len(params) != self.num_params):
+            print("invalid number of params in RXGate")
+
+        self.params = params
+
         return None
 
-    def get_unitary(self, variation_index: int = 0) -> np.ndarray:
-        ct = np.cos(self.variations[variation_index] / 2.0)
-        st = np.sin(self.variations[variation_index] / 2.0)
+    def get_unitary(self) -> np.ndarray:
+        ct = np.cos(self.params[0] / 2.0)
+        st = np.sin(self.params[0] / 2.0)
+
+        return np.matrix([[ct, -1 * st], [st, ct]])
+
+
+class RZGate(Gate):
+    def __init__(self, params=[math.pi / 2.0]) -> None:
+        self.num_qbits = 1
+        self.num_params = 1
+        if (len(params) != self.num_params):
+            print("invalid number of params in RXGate")
+
+        self.params = params
+
+        return None
+
+    def get_unitary(self) -> np.ndarray:
+        return np.matrix([[np.exp(-1j * self.params[0] / 2.0), 0],
+                          [0, np.exp(1j * self.params[0] / 2.0)]])
+
+
+class RXXGate(Gate):
+    def __init__(self, params=[math.pi / 2.0]) -> None:
+        self.num_qbits = 2
+        self.num_params = 1
+        if (len(params) != self.num_params):
+            print("invalid number of params in RXGate")
+
+        self.params = params
+        return None
+
+    def get_unitary(self) -> np.ndarray:
+        ct = np.cos(self.params[0] / 2.0)
+        st = np.sin(self.params[0] / 2.0)
 
         return np.matrix([[ct, 0, 0, -1j * st], [0, ct, -1j * st, 0],
                           [0, -1j * st, ct, 0], [-1j * st, 0, 0, ct]])
 
 
 if __name__ == "__main__":
-    rx = RXGate()
-    rxx = RXXGate()
+    rx = RXGate([-math.pi / 2.0])
+    ry = RYGate([-math.pi / 2.0])
+    ryo = RYGate([math.pi / 2.0])
+    rz = RZGate([-math.pi / 2.0])
+    rxx = RXXGate([math.pi / 2.0])
 
-    circuit = Circuit(4)
-    circuit.append_gate(rx, 1)
+    circuit = Circuit(2, qiskit_style=False)
+    circuit.append_gate(ryo, 0)
     circuit.append_gate(rxx, 0)
-    circuit.append_gate(rx, 2)
+    circuit.append_gate(rx, 0)
+    circuit.append_gate(ry, 0)
+    circuit.append_gate(rx, 1)
+
     print(np.around(circuit.get_unitary(), 2))
