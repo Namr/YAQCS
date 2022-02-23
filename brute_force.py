e6a3b3ffb61387e5e27861e25ae741080f2bf7e0
Amr Moussa <amrmoussa100@gmail.com>
2022-02-23 16:13:10 -0500

    fixed MPI parallelism, changed comparison function

--- brute_force.py
+++ brute_force.py
@@ -1,11 +1,11 @@
-from yaqcs import RXGate, RXXGate, Circuit, RYGate, RZGate
+from yaqcs import RXGate, RXXGate, Circuit, RYGate, RZGate, unitary_distance
 from math import pi, sqrt
 import copy
 import time
 import itertools
 import numpy as np
 
-USING_MPI = False
+USING_MPI = True
 if USING_MPI:
     import threading
     from mpi4py import MPI
@@ -25,7 +25,10 @@ timerS = time.time()
 r2o2 = sqrt(2.0) / 2.0
 ww = r2o2 + r2o2 * 1.0j
 www = -r2o2 + -r2o2 * 1.0j
-target_unitary = [[www, 0, 0, 0], [0, www, 0, 0], [0, 0, 0, ww], [0, 0, ww, 0]]
+#target_unitary = [[-www, 0, 0, 0], [0, -www, 0, 0], [0, 0, 0, -ww], [0, 0, -ww, 0]]
+target_unitary = [[1.0, 0, 0, 0], [0, 1.0, 0, 0], [0, 0, 0, 1.0], [0, 0, 1.0, 0]]
+comm_size = 1
+rank = 0
 '''
 target_unitary = [[
     0.35355 - 0.35355j, -0.35355 + 0.35355j, 0.35355 - 0.35355j,
@@ -54,10 +57,11 @@ def test_circuit(qubits: int, circuit: list) -> bool:
             return False
         c.append_gate(gate[0], gate[1])
 
-    if (np.around(c.get_unitary(), 3) == np.around(target_unitary, 3)).all():
+    #if (np.around(c.get_unitary(), 3) == np.around(target_unitary, 3)).all():
+    if unitary_distance(c.get_unitary(), target_unitary) < 0.01:
         print("============================================")
         print("found a solution with the following unitary")
-        print(c.get_unitary())
+        print(np.around(c.get_unitary(), 3))
         print("created with this circuit")
         print(circuit)
         print("============================================")
@@ -67,26 +71,23 @@ def test_circuit(qubits: int, circuit: list) -> bool:
 
 def pure_brute_force(qubits: int,
                      gates: list,
-                     max_depth: int,
-                     stride: int = 1) -> bool:
+                     max_depth: int) -> bool:
 
     gates_with_positions = list(itertools.product(allowed_gates,
                                                   range(qubits)))
 
-    count = 0
     gCount = 0
-    print(len(list(itertools.product(gates_with_positions, repeat=max_depth))))
+    myCount = 0
+    #print(len(list(itertools.product(gates_with_positions, repeat=max_depth))))
 
     for circuit in itertools.product(gates_with_positions, repeat=max_depth):
-        count = count + 1
         gCount = gCount + 1
-        if gCount % 1000 == 0:
-            print(time.time() - timerS)
-        if count == stride:
-            count = 0
-            if test_circuit(qubits, circuit):
-                return True
-
+        if gCount % comm_size == rank:
+          myCount += 1
+          test_circuit(qubits, circuit)
+    if rank == 0:
+      print(gCount)
+      print(target_unitary)
     return False
 
 
@@ -94,7 +95,8 @@ if __name__ == "__main__":
     if USING_MPI:
         comm = MPI.COMM_WORLD
         rank = comm.Get_rank()
-        pure_brute_force(2, allowed_gates, 3, stride=rank - 1)
+        comm_size = comm.size
+        pure_brute_force(2, allowed_gates, 5)
         comm.Barrier()
     else:
         pure_brute_force(2, allowed_gates, 5)
